1a303f554ea5d6f7ba8967ba85dbeeacf4370a83
Afshin Izadnia <izadnia@yahoo.com>
2024-03-24 04:26:35 +0430

    Calendar moved to the "calendar.service"

--- src/app/modules/calendar/components/calender/calendar.component.ts
+++ src/app/modules/calendar/components/calender/calendar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import moment from 'jalali-moment'; // Importing Jalali-Moment correctly
+import { CalendarService } from '../../../../services/calendar.service';
 
 @Component({
   selector: 'app-calendar',
@@ -7,62 +8,20 @@ import moment from 'jalali-moment'; // Importing Jalali-Moment correctly
   styleUrls: ['./calendar.component.scss'],
 })
 export class CalendarComponent implements OnInit {
+  constructor(private calendarService: CalendarService) {}
+
   calendar: any[][] = [];
+  year: number = 0;
+  monthName: string = '';
 
-  constructor() {}
-
-  jalaliDate = moment(); // Creating a Jalali-Moment object
-  month = this.jalaliDate.jMonth();
-  year = this.jalaliDate.jYear();
-  monthNames = [
-    'فروردین',
-    'اردیبهشت',
-    'خرداد',
-    'تیر',
-    'مرداد',
-    'شهریور',
-    'مهر',
-    'آبان',
-    'آذر',
-    'دی',
-    'بهمن',
-    'اسفند',
-  ];
-  monthName = ''
   ngOnInit(): void {
     this.jalaliCal();
   }
   jalaliCal() {
-    const startDate = moment.jDaysInMonth(this.year, this.month); // Getting the total number of days in the month
-    // Start from the first day of the month
-    let currentDate = moment(startDate)
-      .jYear(this.year)
-      .jMonth(this.month)
-      .jDate(1);
-
-    let weeks: any[] = [];
-    let currentWeek: any[] = [];
-
-    // Generate weeks until the end of the month
-    while (currentDate.jMonth() === this.month) {
-      currentWeek.push(moment(currentDate)); // Using moment() to create a Jalali-Moment object
-      if (currentDate.jDay() === 6 || currentWeek.length === 7) {
-        // 6 corresponds to the end of the week in Jalali calendar
-        weeks.push(currentWeek);
-        currentWeek = [];
-      }
-      currentDate = moment(currentDate).add(1, 'day');
-    }
-
-    // Push the last week
-    if (currentWeek.length > 0) {
-      weeks.push(currentWeek);
-    }
-
-    // console.log('Calendar:', weeks); // Log the calendar to see if it's populated correctly
-
-    this.calendar = weeks;
-    this.monthName = this.monthNames[this.month];
+    this.calendarService.jalaliCal();
+    this.calendar = this.calendarService.calendar;
+    this.year = this.calendarService.year;
+    this.monthName = this.calendarService.monthName;
   }
   monthTranslater() {}
 
@@ -70,26 +29,15 @@ export class CalendarComponent implements OnInit {
     return Array(count).fill(null);
   }
   nextmonth() {
-    if (this.month == 11) {
-      this.month = 0;
-      this.year = this.year + 1;
-    } else {
-      this.month = this.month + 1;
-    }
+    this.calendarService.nextmonth();
     this.jalaliCal();
   }
-  today(){
-    this.month = this.jalaliDate.jMonth();
-    this.year = this.jalaliDate.jYear();
+  today() {
+    this.calendarService.today();
     this.jalaliCal();
   }
   prevmonth() {
-    if (this.month == 0) {
-      this.month = 11;
-      this.year = this.year - 1;
-    } else {
-      this.month = this.month - 1;
-    }
+    this.calendarService.prevmonth();
     this.jalaliCal();
   }
 }
---
+++ src/app/services/calendar.service.ts
@@ -0,0 +1,86 @@
+import { Injectable } from '@angular/core';
+import moment from 'jalali-moment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CalendarService {
+  constructor() {}
+  calendar: any[][] = [];
+  monthName = '';
+  monthNames = [
+    'فروردین',
+    'اردیبهشت',
+    'خرداد',
+    'تیر',
+    'مرداد',
+    'شهریور',
+    'مهر',
+    'آبان',
+    'آذر',
+    'دی',
+    'بهمن',
+    'اسفند',
+  ];
+  jalaliDate = moment(); // Creating a Jalali-Moment object
+  month = this.jalaliDate.jMonth();
+  year = this.jalaliDate.jYear();
+  jalaliCal() {
+    const startDate = moment.jDaysInMonth(this.year, this.month); // Getting the total number of days in the month
+    // Start from the first day of the month
+    let currentDate = moment(startDate)
+      .jYear(this.year)
+      .jMonth(this.month)
+      .jDate(1);
+
+    let weeks: any[] = [];
+    let currentWeek: any[] = [];
+
+    // Generate weeks until the end of the month
+    while (currentDate.jMonth() === this.month) {
+      currentWeek.push(moment(currentDate)); // Using moment() to create a Jalali-Moment object
+      if (currentDate.jDay() === 6 || currentWeek.length === 7) {
+        // 6 corresponds to the end of the week in Jalali calendar
+        weeks.push(currentWeek);
+        currentWeek = [];
+      }
+      currentDate = moment(currentDate).add(1, 'day');
+    }
+
+    // Push the last week
+    if (currentWeek.length > 0) {
+      weeks.push(currentWeek);
+    }
+
+    // console.log('Calendar:', weeks); // Log the calendar to see if it's populated correctly
+
+    this.calendar = weeks;
+    this.monthName = this.monthNames[this.month];
+
+    return this.calendar, this.monthName;
+  }
+  nextmonth() {
+    if (this.month == 11) {
+      this.month = 0;
+      this.year = this.year + 1;
+    } else {
+      this.month = this.month + 1;
+    }
+    this.jalaliCal();
+  }
+  today() {
+    this.month = this.jalaliDate.jMonth();
+    this.year = this.jalaliDate.jYear();
+    this.jalaliCal();
+  }
+  prevmonth() {
+    if (this.month == 0) {
+      this.month = 11;
+      this.year = this.year - 1;
+    } else {
+      this.month = this.month - 1;
+    }
+    this.jalaliCal();
+  }
+}
